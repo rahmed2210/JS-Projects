375acab612d973cb4e91abbec341dd596a59e9bd
Rubal <rahmed2210@gmail.com>
2025-01-20 22:31:58 -0500

    Slot Machine

--- slot_Machine/app.js
+++ slot_Machine/app.js
@@ -28,10 +28,6 @@ const SYMBOL_VALUES = {
 
 
 
-
-
-
-
 // Step one --> collect deposit from user
 const deposit = () => {
     while (true) {
@@ -77,20 +73,79 @@ const getBet = (balance, lines) => {
 // Spin the slot machine
 const spin = () => {
     const symbols = []
-    for (const [symbols, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
         for (let i = 0; i < count; i++) {
-            symbols.push(symbols)
+            symbols.push(symbol)
         }
     }
     
-    const reels = [[], [], []]
+    const reels = []
     for (let i =0; i < COLS; i++) {
-        const reelsSymbols = [...symbols]
+        reels.push([])
+        const reelSymbols = [...symbols]
         for (let j = 0; j < ROWS; j++) {
-            
+            const randomIndex = Math.floor(Math.random() *  reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex]
+            reels[i].push(selectedSymbol)
+            reelSymbols.splice(randomIndex, 1)
         }
     }
+    return reels
 }
+
+//  Transpose the slot machine
+const transpose = (reels) => {
+    const rows = []
+    
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([])
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i])
+        }
+    }
+    
+    return rows
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = ''
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol
+            if (i != row.length - 1) {
+                rowString += ' | '
+            }
+        }
+        console.log(rowString)
+    }
+}
+
+// check if the user won
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0
+    
+    for (let row = 0; row < lines; row++) {
+        const symbol = rows[row]
+        let allSame = true
+        
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false
+                break
+            }
+        }
+        
+        if (allSame) {
+            winnings += bet * SYMBOL_VALUES[symbols[0]]
+        }
+    }
+    
+    return winnings
+}
+
+
+
+
 
 
 
@@ -99,12 +154,11 @@ const spin = () => {
 let balance = deposit()
 const NumberOfLines = getNumberOfLines()
 const bet = getBet(balance, NumberOfLines)
-
-
-
-
-
-
+const reels = spin()
+const rows = transpose(reels)
+printRows(rows)
+const winnings = getWinnings(rows, bet, NumberOfLines)
+console.log('You won, $ ' + winnings.toString())
 
 
 
