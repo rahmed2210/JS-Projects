a5d250f0378f1942fd5be48e1ae1efa6ac70f8d1
Rubal <rahmed2210@gmail.com>
2025-01-22 22:01:35 -0500

    project 1

---
+++ js_tuto/projects1.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync') ()
+const  name = prompt('What is your name? ')
+console.log(name)
+console.log('Hello', name, 'How are you?')
+console.log('Hello', name, 'Welcome to our game!')
+
+const shouldWePlay = prompt('Do you want to play? ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
