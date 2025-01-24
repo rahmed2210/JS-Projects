a586c34cb773b9f444bd7f969343c90c329065d6
Rubal <rahmed2210@gmail.com>
2025-01-23 21:20:15 -0500

    project 1

--- js_tuto/projects1.js
+++ js_tuto/projects1.js
@@ -3,20 +3,26 @@ const  name = prompt('What is your name? ')
 
 const shouldWePlay = prompt('Do you want to play? ')
 
-//   ==>  Game logic
-const leftOrRight = prompt('You enter a maze, do you want to go left or right? ')
-if(leftOrRight.toLowerCase() === 'left') {
-    console.log('You go left and see a bridge..')
-    const cross = prompt('Do you want to cross the bridge? ').toLowerCase()
-    if (cross == 'yes') {
-        console.log('You cross but the bridge was weak and broke and you fell. you lost')
+if (shouldWePlay.toLowerCase() === 'yes') {
+    // Game Logic
+    const leftOrRight = prompt('You enter a maze, do you want to go left or right? ')
+    if (leftOrRight.toLowerCase() === 'left') {
+        console.log('You go left and see a bridge')
+        const cross = prompt('Do you want to cross the bridge? ').toLowerCase()
+        if (cross === 'yes' || cross === 'y') {
+            console.log('You cross but the bridge was weak and broke and you fell. you lost...')
+        } else {
+            console.log('Good choice...You win')
+        }
+        
     } else {
-        console.log('Good choice...You Win')
+        console.log('You go right and fall off a cliff..')
     }
+} else if (shouldWePlay.toLowerCase() === 'no') {
+    console.log('Okay :')
 } else {
-    console.log('You go right and fall off a cliff! YOU lOST')
+    console.log('Invalid input....')
 }
-
 
 
 
@@ -58,6 +64,3 @@ if (shouldWePlay.toLowerCase() === 'yes') {
 }
 
 */
-
-
-//
