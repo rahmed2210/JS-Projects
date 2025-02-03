9c7d8375565ec4b44cf76f6497b9760a397bfb8e
Rubal <rahmed2210@gmail.com>
2025-02-02 22:40:04 -0500

    ToDoApp (Adding elements to DOM's)

--- u-apps/TodoApp/todo-app.js
+++ u-apps/TodoApp/todo-app.js
@@ -1,23 +1,38 @@
 // TODO APP
 
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
 
-const paragraphs = document.querySelectorAll('p')
+// You have 2 todos left (p element)
+// add a p for each todo above (use text value)
 
-paragraphs.forEach(function (paragraph) {
-    if (paragraph.textContent.includes('the')) {
-        paragraph.remove()
-    }
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
 })
 
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
 
-
-
-
-
-
-
-
-
+todos.forEach(function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
 
 
 
