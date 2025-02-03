b4763349fb111d160c8a4f8affbb115fbcebe111
Rubal <rahmed2210@gmail.com>
2025-02-02 22:39:23 -0500

    ToDoApp (Adding elements to DOM's)

---
+++ 1ectures/T2_todo-app.js
@@ -0,0 +1,57 @@
+// TODO APP
+
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+// You have 2 todos left (p element)
+// add a p for each todo above (use text value)
+
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function (todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
