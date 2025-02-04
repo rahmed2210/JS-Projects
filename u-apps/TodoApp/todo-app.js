fc037c39ca6c15e5cd88f58d8a0c8e7e68b70712
Rubal <rahmed2210@gmail.com>
2025-02-03 21:30:03 -0500

    ToDoApp (Advanced Queries)

--- u-apps/TodoApp/todo-app.js
+++ u-apps/TodoApp/todo-app.js
@@ -34,7 +34,7 @@ todos.forEach(function (todo) {
 })
 
 // Listen for new todo creation
-document.querySelector('button').addEventListener('click', function (e) {
+document.querySelector('#add-todo').addEventListener('click', function (e) {
     console.log('Add a new todo..')
 })
 
