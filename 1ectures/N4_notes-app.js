88fdf048036f6046845027de45be70f1f93d4ac8
Rubal <rahmed2210@gmail.com>
2025-02-06 22:22:28 -0500

    NoteApp (Text Input)

---
+++ 1ectures/N4_notes-app.js
@@ -0,0 +1,32 @@
+// NOTE APP   <------->   DOM --> Document Object Model
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise, Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    e.target.textContent = 'This button was clicked'
+})
+
+document.querySelector('#remove-all').addEventListener('click', function () {
+    document.querySelectorAll('.note').forEach(function (note) {
+        note.remove()
+    })
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    console.log(e.target.value)
+})
+
+
+
+
+//
