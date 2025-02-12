a8072fa633d48fe9693eddb9b84353950935812f
Rubal <rahmed2210@gmail.com>
2025-02-11 22:45:30 -0500

    TodoApp (CheckBox)

---
+++ 1ectures/T7_todo-app.js
@@ -0,0 +1,159 @@
+// TODO APP
+
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+const filters = {
+    searchText: '',
+//  create a new hide completed filter (default false)
+    hideCompleted: false 
+}
+
+// renderTodos  -->
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+    
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+
+//------------------------------------------------------------------------------
+/*
+// renderTodos  -->
+const renderTodos = function (todos, filters) {
+    let filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    --> -->   example 2  --> -->
+// Setup renderTodos to remove completed item
+filteredTodos = filteredTodos.filter(function (todo) {
+    return !filters.hideCompleted || !todo.completed
+})
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+*/
+//------------------------------------------------------------------------------
+/* 
+// renderTodos  -->
+const renderTodos = function (todos, filters) {
+    let filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    --> -->   example 1  --> -->
+// Setup renderTodos to remove completed item
+filteredTodos = filteredTodos.filter(function (todo) {
+    if (filters.hideCompleted) {
+        return !todo.completed 
+    } else {
+        return true
+    }
+})
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+*/
+//------------------------------------------------------------------------------
+
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
+
+// Event listener for checkbox
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+//  update hide completed an rerender list on checkbox change
+    filters.hideCompleted = e.target.checked 
+    renderTodos(todos, filters) 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
