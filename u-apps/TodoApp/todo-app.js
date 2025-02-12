cf6a767d3d35a930ce70fe8bf9518ecddb904d3f
Rubal <rahmed2210@gmail.com>
2025-02-11 23:36:22 -0500

    TodoApp (Refactor Challenge)

--- u-apps/TodoApp/todo-app.js
+++ u-apps/TodoApp/todo-app.js
@@ -1,43 +1,10 @@
 // TODO APP
 
-let todos = []
+let todos = getSavedTodos()
 
 const filters = {
     searchText: '',
-//  create a new hide completed filter (default false)
-    hideCompleted: false 
-}
-
-const todosJSON = localStorage.getItem('todos')
-
-if (todosJSON !== null) {
-    todos = JSON.parse(todosJSON)
-}
-
-// renderTodos  -->
-const renderTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-
-        return searchTextMatch && hideCompletedMatch
-    })
-    
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed
-    })
-
-    document.querySelector('#todos').innerHTML = ''
-
-    const summary = document.createElement('h2')
-    summary.textContent = `You have ${incompleteTodos.length} todos left`
-    document.querySelector('#todos').appendChild(summary)
-
-    filteredTodos.forEach(function (todo) {
-        const p = document.createElement('p')
-        p.textContent = todo.text
-        document.querySelector('#todos').appendChild(p)
-    })
+    hideCompleted: false
 }
 
 renderTodos(todos, filters)
@@ -53,19 +20,15 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
         text: e.target.elements.text.value,
         completed: false
     })
-    localStorage.setItem('todos', JSON.stringify(todos))
+    saveTodos(todos)
     renderTodos(todos, filters)
     e.target.elements.text.value = ''
 })
 
-// Event listener for checkbox
 document.querySelector('#hide-completed').addEventListener('change', function (e) {
-//  update hide completed an rerender list on checkbox change
-    filters.hideCompleted = e.target.checked 
-    renderTodos(todos, filters) 
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
 })
-
-
 
 
 
