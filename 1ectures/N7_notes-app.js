b0e79801c28b12273db92414c0aa56719b987695
Rubal <rahmed2210@gmail.com>
2025-02-11 22:44:26 -0500

    NoteApp (CheckBox)

---
+++ 1ectures/N7_notes-app.js
@@ -0,0 +1,67 @@
+// NOTE APP   <------->   DOM --> Document Object Model
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise, Eating a bit better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+
+// filter Objects
+const filters = {
+    searchText: ''
+}
+
+// render
+const renderNotes = function (notes, filter) {
+    const filteredNote = notes.filter(function (notes) {
+        return notes.title.toLowerCase().includes(filter.searchText.toLowerCase())
+    })
+    
+// Duplicate Notes
+    document.querySelector('#notes').innerHTML = ''  
+    
+    filteredNote.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#for-fun').addEventListener('change', function (e) {
+    console.log(e.target.checked)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
