0f1154f0150eb6779e90633801c4269c2fce7ae4
Rubal <rahmed2210@gmail.com>
2025-02-12 22:12:46 -0500

    TodoApp (localStorage)

---
+++ 1ectures/T8_todo-app.js
@@ -0,0 +1,123 @@
+// TODO APP
+
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+
+/*   ------------- Crud Operations --------------
+localStorage.setItem('location', 'Paterson') // setItem -> to Create and update data
+
+console.log(localStorage.getItem('location')) // getItem -> for reading our data
+
+localStorage.removeItem('location')// --> removeItem -> for deleting the application data
+
+localStorage.clear() // --> clear -> for deleting the application data
+*/
+
+/*
+const user = {
+    name: 'rubal',
+    age: 43
+}
+
+// use a method on a global variable called JSON -- Javascript Object Notation
+const userJSON = JSON.stringify(user) // JSON.stringify -> write our object
+console.log(userJSON)
+
+// saving ^ in local storage
+localStorage.setItem('user', userJSON)
+*/
+
+
+// to get data back and actually print the data
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON) // JSON.parse -> to read our object
+console.log(`${user.name} is ${user.age}`)
+
+
+
+
+const filters = {
+    searchText: '',
+//  create a new hide completed filter (default false)
+    hideCompleted: false 
+}
+
+// renderTodos  -->
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+    
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
+
+// Event listener for checkbox
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+//  update hide completed an rerender list on checkbox change
+    filters.hideCompleted = e.target.checked 
+    renderTodos(todos, filters) 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
