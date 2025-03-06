9d1bd3bee47857493c1f57580ca6e25a07f584df
Rubal <rahmed2210@gmail.com>
2025-03-05 22:16:30 -0500

    NoteApp (Syncing Data Across Pages)

--- u-apps/notesapp/notes-app.js
+++ u-apps/notesapp/notes-app.js
@@ -1,6 +1,6 @@
 // NOTE APP   <------->   DOM --> Document Object Model
 
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -31,9 +31,13 @@ document.querySelector('#filter-by').addEventListener('change', function (e) {
     console.log(e.target.value)
 })
 
+window.addEventListener('storage', function (e) {
+    if (e.key == 'notes') {
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
+})
 
-
-// 1. setup link href to include hash with id
 
 
 
