87bcaaf615a57bbe8b934aa15c8a7299eccea53c
Rubal <rahmed2210@gmail.com>
2025-03-05 22:16:46 -0500

    NoteApp (Syncing Data Across Pages)

--- u-apps/notesapp/notes-edit.js
+++ u-apps/notesapp/notes-edit.js
@@ -2,8 +2,8 @@ const titleElement = document.querySelector('#note-title')
 const bodyElement = document.querySelector('#note-body')
 const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function (note) {
+let notes = getSavedNotes()
+let note = notes.find(function (note) {
     return note.id === noteId
 })
 
@@ -30,7 +30,21 @@ removeElement.addEventListener('click', function (e) {
     location.assign('./index.html')
 })
 
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function (note) {
+            return note.id === noteId
+        })
 
+        if (note === undefined) {
+            location.assign('./index.html')
+        }
+
+        titleElement.value = note.title
+        bodyElement.value = note.body
+    }
+})
 
 
 
