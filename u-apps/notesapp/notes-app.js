79c58a8950edb0c19cb9b8ecd8f4cff19dce462a
Rubal <rahmed2210@gmail.com>
2025-03-06 23:00:06 -0500

    NoteApp (Moments)

--- u-apps/notesapp/notes-app.js
+++ u-apps/notesapp/notes-app.js
@@ -38,47 +38,10 @@ window.addEventListener('storage', function (e) {
     }
 })
 
-// JavaScript Dates
-// Unix Epoch
-const now = new Date('January 29 2021 6:25:01')
-const timestamp = now.getTime()
-
-const myDate = new Date(timestamp)
-console.log(myDate.getFullYear())
-
-/*
+const now = moment()
 console.log(now.toString())
 
-console.log(`Year: ${now.getFullYear()}`)
-console.log(`Month: ${now.getMonth()}`)
-console.log(`Day of Month: ${now.getDay()}`)
-
-console.log(`Hours: ${now.getHours()}`)
-console.log(`Minute: ${now.getMinutes()}`)
-console.log(`Second: ${now.getSeconds()}`)
-
-console.log(now.getTime())
-*/
-
-
-// 1. Create two dates in the past (use String for date)
-// 2. Get timestamp for both
-// 3. Figure out which is first and print its time (use toString)
-
-const dateOne = new Date('March 06 2021 12:00:00')
-
-const dateTwo = new Date()
-
-const DateOneTimestamp = dateOne.getTime()
-const DateTwoTimestamp = dateTwo.getTime()
-
-if (DateOneTimestamp < DateTwoTimestamp) {
-    console.log(dateOne.toString())
-} else if (DateTwoTimestamp < DateOneTimestamp) {
-    console.log(dateTwo.toString())
-}
-
-
+console.log(now.toString())
 
 
 
