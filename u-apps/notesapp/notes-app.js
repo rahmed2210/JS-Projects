8ad37a1d680e52d8d4822697be8eb669c0f1f3ab
Rubal <rahmed2210@gmail.com>
2025-03-13 22:36:56 -0400

    NoteApp (Updating regular function to arrow => function)

--- u-apps/notesapp/notes-app.js
+++ u-apps/notesapp/notes-app.js
@@ -9,7 +9,7 @@ const filters = {
 
 renderNotes(notes, filters)
 
-document.querySelector('#create-note').addEventListener('click', function (e) {
+document.querySelector('#create-note').addEventListener('click', (e) => {
     const id = uuidv4()
     const timestamp = moment().valueOf() // Add createdAt and updateAt to the new notes (store timestamp)
     
@@ -27,17 +27,17 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
 //    location.assign('./edit.html')
 })
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
+document.querySelector('#search-text').addEventListener('input', (e) => {
     filters.searchText = e.target.value
     renderNotes(notes, filters)
 })
 
-document.querySelector('#filter-by').addEventListener('change', function (e) {
+document.querySelector('#filter-by').addEventListener('change', (e) => {
     filters.sortBy = e.target.value
     renderNotes(notes, filters)
 })
 
-window.addEventListener('storage', function (e) {
+window.addEventListener('storage', (e) => {
     if (e.key == 'notes') {
         notes = JSON.parse(e.newValue)
         renderNotes(notes, filters)
