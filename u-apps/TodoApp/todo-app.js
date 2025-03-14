193681373cdf9d8cc73f5c83f1639db71b25ed4a
Rubal <rahmed2210@gmail.com>
2025-03-13 22:35:10 -0400

    TodoApp (Updating regular function to arrow => function)

--- u-apps/TodoApp/todo-app.js
+++ u-apps/TodoApp/todo-app.js
@@ -9,12 +9,12 @@ const filters = {
 
 renderTodos(todos, filters)
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
+document.querySelector('#search-text').addEventListener('input', (e) => {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 })
 
-document.querySelector('#new-todo').addEventListener('submit', function (e) {
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
     todos.push({
         id: uuidv4(),
@@ -26,7 +26,7 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
     e.target.elements.text.value = ''
 })
 
-document.querySelector('#hide-completed').addEventListener('change', function (e) {
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
     filters.hideCompleted = e.target.checked
     renderTodos(todos, filters)
 })
