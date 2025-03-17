839f77293cba5abf14274fbe91e1c0f95aecee38
Rubal <rahmed2210@gmail.com>
2025-03-16 21:55:24 -0400

    NoteApp (Conditinal Operator)

--- u-apps/notesapp/notes-functions.js
+++ u-apps/notesapp/notes-functions.js
@@ -1,12 +1,8 @@
 // Read existing notes from localStorage
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem('notes')
-
-    if (notesJSON !== null) {
-        return JSON.parse(notesJSON)
-    } else {
-        return []
-    }
+    
+    return notesJSON !== null ? JSON.parse(notesJSON) : []
 }
 
 // Save the notes to localStorage
