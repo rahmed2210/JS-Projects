ccadb1e1688328382faa82c4a346b247eab84ad7
Rubal <rahmed2210@gmail.com>
2025-03-18 21:01:06 -0400

    NoteApp (Moment)

---
+++ 1ectures/N17_notes-app.js
@@ -0,0 +1,50 @@
+// NOTE APP   <------->   DOM --> Document Object Model
+
+let notes = getSavedNotes()
+
+const filters = {
+    searchText: ''
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    const id = uuidv4()
+    
+    notes.push({
+        id: id,
+        title: '',
+        body: ''
+    })
+    saveNotes(notes)
+    location.assign(`./edit.html#${id}`)
+// 2. ^^ setup the assign call to include hash with id
+//    location.assign('./edit.html')
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
+
+window.addEventListener('storage', function (e) {
+    if (e.key == 'notes') {
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
+})
+
+const now = moment()
+console.log(now.toString())
+
+console.log(now.toString())
+
+
+
+
+
+//
